65210c0e260677ec3a1f25934666a297138aa7b1
emagodi <magodiedwin@gmail.com>
2025-01-19 10:12:10 +0200

    Initial commit

---
+++ src/main/java/zw/co/zetdc/businessplanning/BusinessplanningApplication.java
@@ -0,0 +1,13 @@
+package zw.co.zetdc.businessplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessplanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessplanningApplication.class, args);
+	}
+
+}
