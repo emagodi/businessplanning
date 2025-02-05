48b1385e38c844b294b70cb7cfa81b0703421cc9
emagodi <magodiedwin@gmail.com>
2025-02-05 14:34:57 +0200

    add security for roles on endpoints

--- src/main/java/zw/co/zetdc/businessplanning/controller/ActivityController.java
+++ src/main/java/zw/co/zetdc/businessplanning/controller/ActivityController.java
@@ -3,9 +3,9 @@ package zw.co.zetdc.businessplanning.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import zw.co.zetdc.businessplanning.entities.Activity;
 import zw.co.zetdc.businessplanning.payload.request.ActivityRequest;
@@ -24,12 +24,14 @@ public class ActivityController {
     private final ActivityService activityService;
 
     @PostMapping(value = "/create")
+    @PreAuthorize("hasAuthority('READ_PRIVILEGE') and hasAnyRole('ADMIN', 'SMITIRID' ,'MANAGER', 'HEAD')")
     public ResponseEntity<Activity> createActivity(@RequestBody ActivityRequest activityRequest) {
         Activity createdActivity = activityService.createActivity(activityRequest);
         return new ResponseEntity<>(createdActivity, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "findById/{id}")
+    @PreAuthorize("hasAuthority('READ_PRIVILEGE') and hasAnyRole('ADMIN', 'SMITIRID' ,'MANAGER', 'HEAD')")
     public ResponseEntity<Activity> getActivityById(@PathVariable Long id) {
         Activity activity = activityService.getActivityById(id);
         if (activity != null) {
@@ -40,12 +42,14 @@ public class ActivityController {
     }
 
     @GetMapping(value = "findAll")
+    @PreAuthorize("hasAuthority('READ_PRIVILEGE') and hasAnyRole('ADMIN', 'SMITIRID' ,'MANAGER', 'HEAD')")
     public ResponseEntity<List<Activity>> getAllActivities() {
         List<Activity> activities = activityService.getAllActivities();
         return new ResponseEntity<>(activities, HttpStatus.OK);
     }
 
     @PutMapping(value = "update/{id}")
+    @PreAuthorize("hasAuthority('READ_PRIVILEGE') and hasAnyRole('ADMIN', 'SMITIRID' ,'MANAGER', 'HEAD')")
     public ResponseEntity<Activity> updateActivity(@PathVariable Long id,
                                                    @RequestBody ActivityRequest activityRequest) {
         Activity updatedActivity = activityService.updateActivity(id, activityRequest);
@@ -57,6 +61,7 @@ public class ActivityController {
     }
 
     @DeleteMapping(value ="delete/{id}")
+    @PreAuthorize("hasAuthority('READ_PRIVILEGE') and hasAnyRole('ADMIN', 'SMITIRID' ,'MANAGER', 'HEAD')")
     public ResponseEntity<Void> deleteActivity(@PathVariable Long id) {
         activityService.deleteActivity(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
