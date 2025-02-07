35a185f84bfe07b8dbcb94c0778c52259ce1765e
emagodi <magodiedwin@gmail.com>
2025-02-08 00:13:15 +0200

    And link with time member temporarily

--- src/main/java/zw/co/zetdc/businessplanning/entities/Activity.java
+++ src/main/java/zw/co/zetdc/businessplanning/entities/Activity.java
@@ -1,13 +1,13 @@
 package zw.co.zetdc.businessplanning.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import zw.co.zetdc.businessplanning.handlers.BaseEntity;
 
-
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @EqualsAndHashCode(callSuper = true)
@@ -23,11 +23,17 @@ public class Activity extends BaseEntity {
     private String activityName;
     private Double weeklyTarget;
     private Double actualWorkDone;
-//    private String teamMembers;
     private Double percentageComplete;
     private Double actualExpenditure;
     private Double percentOfBudget;
     private String remarks;
 
+    @ManyToMany
+    @JoinTable(
+            name = "activity_team_member", // This is the junction table name
+            joinColumns = @JoinColumn(name = "activity_id"), // Foreign key for Activity
+            inverseJoinColumns = @JoinColumn(name = "team_member_id") // Foreign key for TeamMember
+    )
+    private List<TeamMember> assignedTeamMembers;
 
 }
